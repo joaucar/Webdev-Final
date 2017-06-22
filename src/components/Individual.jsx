74a6831813fb118218d0e7e27dee2c75da6ff54d
JordanBlock <jordanblock97@gmail.com>
2017-06-22 12:20:06 +0200

    vision

--- src/components/Individual.jsx
+++ src/components/Individual.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import $ from 'jquery';
 import {Icon} from 'react-fa';
-import Map from "./Map"
+import Map from './Map'
+import Emojis from './Emojis'
 //import Sticky from 'react-sticky-el';
 
 const ALCHEMY_API_KEY = "1Ccn8-u7IJ1XoEdIioWyh63a5BSuCik9vDa6s-MesYjb"
 
 class Individual extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-          individual: []
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      individual: []
+    };
+  }
 
-    /*
+  /*
     This method will be called by React after the first render. It's a perfect place to load
     data with AJAX. This User component gets mounted in the DOM as soon as the URL is /user/:username
 
@@ -27,40 +28,66 @@ class Individual extends React.Component {
     When `render` gets called again, `this.state.user` exists and we get the user info display instead of "LOADING..."
     */
 
-    componentDidMount() {
-      this.fetchData()
+  componentDidMount() {
+    this.fetchData()
+  }
 
-      fetch(`https://watson-api-explorer.mybluemix.net/alchemy-api/calls/text/TextGetRankedKeywords?apikey=${ALCHEMY_API_KEY}&text=hi%20my%20name%20is%20jordan`)
+  fetchData = () => {
+    console.log('location', this.props)
+    const accessTOK = this.props.location.hash.substr(1)
+    $.ajax({
+      url: `https://api.instagram.com/v1/users/self/media/recent/?${accessTOK}`,
+      jsonp: "callback",
+      dataType: "jsonp",
+      success: (response) => {
+        this.mergeWithVisionData(response.data)
+      }
+    })
+  }
+
+  mergeWithVisionData = (images) => {
+    images.forEach((image) => {
+      const request = {
+         "requests":[
+           {
+             "image":{
+              "source": {
+                "imageUri": image.images.standard_resolution.url
+              }
+              },
+             "features":[
+               {
+                 "type":"FACE_DETECTION"
+               },
+               {
+                 "type": "SAFE_SEARCH_DETECTION"
+               }
+             ]
+           }
+         ]
+       };
+      fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBUOfIfVpDCIIYz9n7cCbMpHPD3msBZ_ig', {
+          method: 'POST',
+          headers: {
+           'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(request)
+        })
         .then((raw) => raw.json())
         .then((res) => {
           console.log('res', res)
-        })
-    }
+          this.setState({
+            individual: this.state.individual.concat([{
+              ...image,
+              visionData: res
+            }])
+          })
+        }).catch((e) => console.log('error', e))
+    })
+  }
 
-    fetchData = () => {
-      console.log('location', this.props)
-        const accessTOK =  this.props.location.hash.substr(1)
-        $.ajax({
-          url: `https://api.instagram.com/v1/users/self/media/recent/?${accessTOK}`,
-          jsonp: "callback",
-          dataType: "jsonp",
-          success: function( response ) {
-            console.log( 'Individual data', response );
-          },
-        })
-        .then((data) => {
-        //         // How can we use `this` inside a callback without binding it??
-        //         // Make sure you understand this fundamental difference with arrow functions!!!
-                 this.setState({
-                     individual: data.data,
-                 });
-                  console.log('data', this.state.individual)
-            })
-    }
-
-
-
-  render () {
+  render() {
+    console.log('this state', this.state)
     if (!this.state.individual) {
       return <div>LOADING...</div>
     }
@@ -71,20 +98,20 @@ class Individual extends React.Component {
             return (
               <div className="pics" key={i}>
                 <div className="locations">{individual.location === null
-                  ? <p>  </p>
-                  : <p><Icon name="map-marker" />       {individual.location.name}</p>}
+                    ? <p></p>
+                    : <p><Icon name="map-marker"/> {individual.location.name}</p>}
                 </div>
-                <img role="presentation" className="instagramimages" src={individual.images.standard_resolution.url} />
+                <img role="presentation" className="instagramimages" src={individual.images.standard_resolution.url}/>
                 <div className="caption">
                   {individual.caption === null
-                    ? <p>  </p>
+                    ? <p></p>
                     : <p>{individual.caption.text}</p>}
                 </div>
               </div>
             )
           })}
         </div>
-        <Map {...this.props} />
+        <Map {...this.props}/>
       </div>
     )
 
